22853b55e7cc9031c345a1cc2e13a575b6571fa9
Andert51 <andrest05143@gmail.com>
2024-08-24 11:25:27 -0600

    Cors support added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import employeRoutes from './routes/employeRoutes.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Content-type' , 'Authorization' ]
+}
+
+app.use(cors(corsOptions)) // Es para que de cualquier medio se pueda acceder a las consultas
 app.use(express.json()) // Alternativa de body parser
 app.use('/api/employe', employeRoutes) //Peticion del cliente al dominio para que el back responda
 
